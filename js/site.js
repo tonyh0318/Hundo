ddc27c978ff54e5c18fb2789c46939ca94701e51
tonyh0318 <96548981+tonyh0318@users.noreply.github.com>
2022-01-18 10:19:17 -0800

    msg

--- js/site.js
+++ js/site.js
@@ -1,13 +1,51 @@
-//display a message to the user
-function getMessage() {
+//Display message to the user
+function getValues(){
+    let startValue = document.getElementById("startValue").value;
+    let endValue = document.getElementById("endValue").value;
 
-    let userMessage = document.getElementById("message").value;
-    
-    Swal.fire(
-        'The Internet?',
-        'That thing is still around?',
-        'question'
-    )
-    
-    console.log(userMessage);
+    startValue = parseInt(startValue);
+    endValue = parseInt(endValue);
+
+    //Validate Input
+    if(Number.isInteger(startValue) && Number.isInteger(endValue)){
+        let numArray = generateNumbers(startValue, endValue);
+
+        displayNumbers(numArray);
+    }
+    else{
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Only integers are allowed for Hundo'
+        });
+    }
+}
+
+function generateNumbers(sVal, eVal){
+
+    let numArray = [];
+    for (let i = sVal; i <= eVal; i++){
+        numArray.push(i);
+    }
+
+    return numArray;
+}
+
+function displayNumbers(numArray){
+ 
+    let templateRows = "";
+    let className = "";
+    for (let i = 0; i < numArray.length; i++){
+
+        if (numArray[i] % 2 == 0){
+            className = "even";
+        } else {
+            className = "odd";
+        }
+
+        let row = `<tr><td class="${className}">${numArray[i]}</td></tr>`;
+        templateRows += row;
+    }
+
+    document.getElementById("results").innerHTML = templateRows;
 }
